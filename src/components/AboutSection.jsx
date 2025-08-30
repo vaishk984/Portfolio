89f52f97a9b599508d0601d62dfa2b669b93aca5
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-08-30 23:50:46 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ const AboutSection = () => {
               </a>
 
               <a
-                href="./public/Kumari-Vaishnavi-Resume.pdf"
+                href="https://drive.google.com/file/d/1mezxskWkRVr-ErefsNXDEJunWM3YrGx0/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
